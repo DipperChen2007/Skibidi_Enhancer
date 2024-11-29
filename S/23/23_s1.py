94d1b643852a45ff8a627e44715e49f4654dd3c5
DipperChen2007 <chenxingan65@gmail.com>
2024-11-28 16:05:49 -0800

    ccc

---
+++ S/23/23_s1.py
@@ -0,0 +1,12 @@
+def Trianglane(n,up_lst,down_lst):
+    pass
+            
+            
+
+def take_input():
+    n = int(input())
+    up_lst = list(map(int,input().split()))
+    down_lst = list(map(int,input().split()))
+    return n,up_lst,down_lst
+
+n,up_lst,down_lst = take_input()
