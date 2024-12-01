66fc84db4de263fb2fb919e634fc0c291523e3fa
DipperChen2007 <chenxingan65@gmail.com>
2024-11-30 22:24:47 -0800

    ccc

---
+++ S/23/23_s2.py
@@ -0,0 +1,30 @@
+def Symmetric_Mountains(mountains):
+    answer_lst = []
+    for i in range(len(mountains)):
+        answer = find_symmetric(i+1,mountains)
+        answer_lst.append(answer)
+    print(*answer_lst)
+        
+def find_symmetric(i,mountains):
+    answer = float('inf')
+    if i == 1:
+        return 0
+    for j in range(len(mountains) - i + 1):
+        sub_lst = mountains[j:j + i]
+        abs_vlaue = 0
+        for l in range(len(sub_lst)//2):
+            a = abs(sub_lst[l] - sub_lst[-l - 1])
+            abs_vlaue += a 
+        
+        answer = min(answer,abs_vlaue)
+    return answer 
+        
+        
+def take_input():
+    n = int(input())
+    mountains = list(map(int,input().split()))
+    return mountains
+
+mountains = take_input()
+Symmetric_Mountains(mountains)
+    
---
+++ S/23/23_s4.py
@@ -0,0 +1,14 @@
+# remove unnececery roads
+
+# 1. sort roads decendent从大到小
+# 优先删除最贵的路
+
+# 2 loop roads
+    # if a -> b has a faster detour (*1)
+        # remove road
+    # else:
+        # add cost`
+
+# return cost
+
+# *1: use dijkstra to calculate the shortest path
