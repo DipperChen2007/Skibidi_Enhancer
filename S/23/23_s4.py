057397228d144464752bb47e59b387c6aa2953fc
DipperChen2007 <chenxingan65@gmail.com>
2024-12-02 11:10:43 -0800

    ccc

--- S/23/23_s4.py
+++ S/23/23_s4.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 # remove unnececery roads
 
 # 1. sort roads decendent从大到小
@@ -12,3 +13,19 @@
 
 # return cost
 # *1: use dijkstra to calculate the shortest paths
+
+def take_input():
+    n,m = map(int,input().split())
+    lst = []
+    dic = defaultdict(list)
+    for _ in range(m):
+        lst.append(list(map(int,input().split())))
+    lst.sort(key = lambda x:x[3])
+    for i in range(m):
+        p1,p2,l,c = lst[i]
+        dic[p1].append((p2,l,c))
+        dic[p2].append((p1,l,c))
+    return n,m,dic
+
+n,m,dic = take_input()
+print(n,m,dic)
