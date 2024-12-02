2d2dcb7827157f020e3f3da6d9cbc4ac0df2a71a
DipperChen2007 <chenxingan65@gmail.com>
2024-12-01 21:23:24 -0800

    ccc

--- S/22/22_s1.py
+++ S/22/22_s1.py
@@ -1,28 +1,11 @@
-from collections import deque
-
 def g4_5(n):
-    if n == 4:
-        return 1 
-    elif n < 5:
-        return 0
-    
-    visited = set()
-    frontier = deque([n])
-    visited.add(n)
     answer = 0
-    while frontier:
-        left_over = frontier.popleft()
-        for i in [4,5]:
-            if left_over - i == 0:
-                answer += 1
-                break
-            elif left_over - i > 0 and (left_over - i) not in visited:
-                frontier.append(left_over - i)
-                print(frontier)
-            else:
-                break
-    return answer 
-    
+    for i in range(1000001):
+        five = i*5
+        remain = n  - five 
+        if remain >= 0 and remain%4 == 0:
+            answer += 1
+    return answer
 
 def take_input():
     n = int(input())
