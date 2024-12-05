6aca2bde19ad48228e4c1c4c0700fae965bfbc0d
DipperChen2007 <chenxingan65@gmail.com>
2024-12-05 05:22:27 -0800

    ccc

--- S/23/23_s4.py
+++ S/23/23_s4.py
@@ -13,7 +13,7 @@ from collections import defaultdict
 
 # return cost
 # *1: use dijkstra to calculate the shortest paths
-def MST(n,m,lst):
+def MST(n,m,dic):
     edges = 0
     parent = []
     rank = []
@@ -25,9 +25,28 @@ def MST(n,m,lst):
     total_edges = n 
     
     while edges < total_edges:
-        p1,p2,l,c = lst[i]
-        i += 1
-        pass
+        p1 = i
+        for j in range(len(dic[i])):
+            p2,l,c = dic[i][j]
+            p1_parent = find_parent[parent,p1]
+            p2_parent = find_parent[parent,p2]
+            if p1_parent != p2_parent:
+                e += 1
+                result.append(c)
+                Update_parent(parent,p1,p2,rank)
+            
+    return sum(result)
+
+def Update_parent(parent,u,v,rank):
+    if rank[u] < rank[v]:
+        parent[u] = v
+    elif rank[u] > rank[v]:
+        parent[v] = u 
+    else:
+        parent[u] = v
+        rank[u] += 1
+        rank[v] = rank[u]
+        
 
 
 def find_parent(parent,child):
@@ -40,10 +59,16 @@ def find_parent(parent,child):
 def take_input():
     n,m = map(int,input().split())
     lst = []
+    dic = defaultdict(list)
     for _ in range(m):
         lst.append(list(map(int,input().split())))
     lst.sort(key = lambda x:x[3])
-    return n,m,lst
+    for i in range(m):
+        p1,p2,l,c = lst[i]
+        dic[p1].append([p2,l,c])
+        dic[p2].append([p1,l,c])
+    return n,m,dic
 
-n,m,lst = take_input()
-print(n,m,lst)
+n,m,dic = take_input()
+print(dic)
+print(MST(n,m,dic))
