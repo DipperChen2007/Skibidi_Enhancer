b1c0b69d4e946fa79ab5249a68090f4a6fef24be
DipperChen2007 <chenxingan65@gmail.com>
2024-12-08 20:05:01 -0800

    ccc

--- S/Djisktra_Practice.py
+++ S/Djisktra_Practice.py
@@ -44,6 +44,32 @@ def bfs(graph, target):
     
     return -1
 
+graph1 = {
+    'a': [(3, 'b'), (2, 'd'), (5, 'c')],
+    'b': [(3, 'a'), (1, 'c')],
+    'c': [(1, 'b'), (2, 'd'), (5, 'a')],
+    'd': [(2, 'a'), (2, 'c')]
+}
+# MST 于BFS, DFS之间最大的区别在于每一个edge都带有一个weight(长度)，我们需要根据长度来决定使用哪条边
+# starting from 'a', find the minimum total length to connect the entire graph
+# 从a点出发， 拓展相邻的点和线，放在heap里面，按照长度排序
 
+# heap不同于stack和queue，放进去的东西永远会按照从小到大的顺序排列好，insert的速度是O(n^2)
+# heappop会取出里面的第0个element，所以heap永远会取出最小的
+
+# init heap[(0, 'a')]
+# expand a: heap[(2, 'd'), (3, 'b'), (5, 'c')] total += 0
+# expand d: heap[(2, 'a'), (2, 'c'), (3, 'b'), (5, 'c')] total += 2
+# expand a: heap[(2, 'c'), (3, 'b'), (5, 'c')] a visited
+# expand c: heap[(1, 'b'), (2, 'd'), (3, 'b'), (5, 'c'), (5, 'a')] total += 2
+# expand b: heap[(1, 'c'), (2, 'd'), (3, 'b'), (3, 'a'), (5, 'c'), (5, 'a')] total += 1
+# expand c: heap[]
+import heapq
+def MST(graph, target):
+    hp = []
+    heapq.heappush(hp,(0,'a'))
+    
+    
+    
 def djisktra(graph):
     pass
