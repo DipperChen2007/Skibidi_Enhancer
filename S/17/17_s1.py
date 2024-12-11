ae511ae034892448c68915ecffc5ddb0711b86cd
DipperChen2007 <chenxingan65@gmail.com>
2024-12-11 10:46:49 -0800

    ccc

---
+++ S/17/17_s1.py
@@ -0,0 +1,12 @@
+def Sum_Game(n,team_1,team_2):
+    dp = [0]*n
+    
+
+
+def take_input():
+    n = int(input())
+    team_1 = list(map(int,input().split()))
+    team_2 = list(map(int,input().split()))
+    return n,team_1,team_2
+
+n,team_1,team_2 = take_input()
