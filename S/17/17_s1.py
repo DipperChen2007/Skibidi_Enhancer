05c811a9c5a434c1aa5e5df12c761f5f4a84291b
DipperChen2007 <chenxingan65@gmail.com>
2024-12-11 12:20:08 -0800

    ccc

--- S/17/17_s1.py
+++ S/17/17_s1.py
@@ -1,8 +1,19 @@
 def Sum_Game(n,team_1,team_2):
-    dp = [0]*n
-    
-
-
+    dp = [0]*(n + 1)
+    sum_1 = 0
+    sum_2 = 0
+    dp[0] = 1
+    for i in range(n):
+        sum_1 += team_1[i]
+        sum_2 += team_2[i]
+        if sum_1 == sum_2:
+            dp[i+1] += 1
+    answer = 0
+    for j in range(n + 1):
+        if dp[j] == 1:
+            answer = max(answer,j)
+    return answer            
+        
 def take_input():
     n = int(input())
     team_1 = list(map(int,input().split()))
@@ -10,3 +21,4 @@ def take_input():
     return n,team_1,team_2
 
 n,team_1,team_2 = take_input()
+print(Sum_Game(n,team_1,team_2))
