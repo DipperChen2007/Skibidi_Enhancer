47b98bf92403fc1fa66294bdcb54dc5ed0f2b917
DipperChen2007 <chenxingan65@gmail.com>
2024-12-15 18:57:26 -0800

    ccc

---
+++ Algos/Meeting_rooms.py
@@ -0,0 +1,14 @@
+from collections import deque
+def Meeting_rooms(lst):
+    lst.sort(key = lambda x:x[1])
+    frontier = deque([])
+    answer = 0
+    for start,end in lst:
+        frontier.append((start,end))
+        while start >= frontier[0][1]:
+            frontier.popleft()
+        answer = max(len(frontier),answer)   
+    return answer
+
+lst = [[7,10],[2,4]]
+print(Meeting_rooms(lst))               
