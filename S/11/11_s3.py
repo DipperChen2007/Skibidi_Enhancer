7c1e11e561da6af7da9cea4e94f0043d52a5f076
DipperChen2007 <chenxingan65@gmail.com>
2025-01-17 09:55:59 -0800

    ccc

--- S/11/11_s3.py
+++ S/11/11_s3.py
@@ -1,21 +1,26 @@
 #[[1, 1, 1], [1, 1, 0], [1, 2, 1], [2, 8, 5]]
+POINTS_1 = {(1,0),(2,0),(3,0),(2,1) }
+POINTS_2 = { (1,1),(2,2),(3,1) }
 
 def looking_glass(lst):
-    pass
+    for magnification,p_x,p_y in lst:
+        if check_func(magnification,p_x,p_y):
+            print("crystal")
+        else:
+            print("empty")
 
 def check_func(magnification,p_x,p_y):
+    
     if magnification == 1:
-        if p_x == 1 and p_x == 3:
-            if p_y == 1:
-                return True
-        elif p_x == 2:
-            if p_y == 1 or p_y == 2:
-                return True
-        else:
-            return False
-    elif magnification > 1:
-        
-            
+        return (p_x,p_y) in POINTS_1
+    p_x_copy,p_y_copy = p_x//5,p_y//5
+    if (p_x_copy,p_y_copy) in POINTS_1:
+        return True
+    # p_x_copy,p_y_copy = p_x%5,p_y%5
+    if (p_x_copy,p_y_copy) in POINTS_2:
+        return check_func(magnification - 1,p_x%5,p_y%5)
+    
+    return False            
 
 def take_input():
     n = int(input())
@@ -25,4 +30,4 @@ def take_input():
     return lst
 
 lst = take_input()
-print(lst)
+looking_glass(lst)
