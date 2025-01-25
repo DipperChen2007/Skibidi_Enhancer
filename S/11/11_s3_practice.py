e438e769cc09028c61d9b270d19c05c595794e2e
DipperChen2007 <chenxingan65@gmail.com>
2025-01-24 18:25:17 -0800

    c c c

--- S/11/11_s3_practice.py
+++ S/11/11_s3_practice.py
@@ -1,6 +1,35 @@
+grid = [
+    [0,0,0,0,0],
+    [1,0,0,0,0],
+    [1,1,0,0,0],
+    [1,0,0,0,0],
+    [0,0,0,0,0]
+]
+
+def print_result(points):
+    for magnitude, x, y in points:
+        print(looking_glass(magnitude, x, y))
+
+def looking_glass(magnitude, x, y):
+    # Base case
+    if magnitude == 1:
+        if grid[x][y] == 1:
+            return "crystal"
+        else:
+            return "empty"
+
+    new_size = 5 ** (magnitude - 1)
+    new_x = x % new_size
+    new_y = y % new_size
+
+    return looking_glass(magnitude - 1, new_x, new_y)
+
 def take_input():
     n = int(input())
     points = []
     for _ in range(n):
-        points.append(list(map(int,input().split())))
-    
+        points.append(list(map(int, input().split())))
+    return points
+
+points = take_input()
+print_result(points)
