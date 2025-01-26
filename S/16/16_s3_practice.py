5f1dff82cb0e5e7f4c996cd854e0f5406a1cbe13
DipperChen2007 <chenxingan65@gmail.com>
2025-01-26 11:30:46 -0800

    ccc

--- S/16/16_s3_practice.py
+++ S/16/16_s3_practice.py
@@ -1,7 +1,22 @@
 from collections import defaultdict
+import sys
+sys.setrecursionlimit(10**5)
 
+def dfs(start, graph, N):
+    dist = [-1] * N
+    dist[start] = 0
 
-
+    stack = [(start, -1)]
+    while stack:
+        cur, prev = stack.pop()
+        for adj in graph[cur]:
+            if adj != prev:
+                stack.append((adj, cur))
+                dist[adj] = dist[cur] + 1
+    far = max(dist)
+    return far, dist.index(far)
+    
+    
 def pruning(N, graph, pho):
     for node in range(N):
         while len(graph[node]) == 1 and node not in pho:
@@ -11,6 +26,7 @@ def pruning(N, graph, pho):
             graph[node].remove(prev)
             
 def take_input():
+    input = sys.stdin.readline
     n,m = map(int,input().split())
     phos = list(map(int,input().split()))
     dic = defaultdict(list)
@@ -18,6 +34,13 @@ def take_input():
         a,b = map(int,input().split())
         dic[a].append(b)
         dic[b].append(a)
-    return phos,dic
+    return n,phos,dic
 
-phos,dic = take_input()
+n,phos,dic = take_input()
+pruning(n, dic, phos)
+d,node = dfs(phos[0],dic,n)
+d,node = dfs(node,dic,n)
+two_n = 0
+for key,value in dic.items():
+    two_n += len(value)
+print(two_n - d)
