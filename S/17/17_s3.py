f0eca001296e947d3c047db7e862ec848e4b18ab
DipperChen2007 <chenxingan65@gmail.com>
2025-01-30 17:33:51 -0800

    ccc

--- S/17/17_s3.py
+++ S/17/17_s3.py
@@ -1,21 +1,21 @@
 from collections import Counter
 def Nailed_It(n,lst):
     times = Counter(lst)
-    maximum = max(times) * 2
-    numbers = list(times.keys())
-    dp = [0] * (maximum + 1)
+    maximum_height = max(times) * 2
+    numbers = list(set(lst))
+    dp = [0] * (maximum_height + 1)
     for i in range(len(numbers)):
         for j in range(i,len(numbers)):
-            print(numbers[i],numbers[j]) 
+            
             if i == j:
-                dp[numbers[i] + numbers[j]] = min(times[numbers[i]],times[numbers[j]])//2
+                dp[numbers[i] + numbers[j]] += min(times[numbers[i]],times[numbers[j]])//2
             else:               
-                dp[numbers[i] + numbers[j]] = min(times[numbers[i]],times[numbers[j]])
+                dp[numbers[i] + numbers[j]] += min(times[numbers[i]],times[numbers[j]])
             
     length = max(dp)
-    print(dp)
     height = dp.count(length)  
     print(length,height)
+
                             
 def Take_input():
     n = int(input())
